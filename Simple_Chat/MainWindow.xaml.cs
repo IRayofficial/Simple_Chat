4202513fa195b4f151c171ed5f01ca3f7f410fbf
IRayofficial <oliverhollenbach00@gmail.com>
2024-09-12 21:12:42 +0200

    Add Login add Encryption and Add User Selection

--- Simple_Chat/MainWindow.xaml.cs
+++ Simple_Chat/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -18,10 +19,20 @@ namespace Simple_Chat
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string MyUserName;
         static int[] PrivateKey;
         public string PublicKey;
         public string PartnerPublicKey;
         public string SharedKey;
+        public bool connected = false;
+
+        public string RecievedContentPattern = @"<type>(.*?)</type>|<content>(.*?)</content>";
+        public string GetMessage = @"<username>(.*?)</username>|<message>(.*?)</message>";
+        public string GetUserInfo = @"<username>(.*?)</username>|<public-key>(.*?)</public-key>";
+        public string GetClient = @"<client>(.*?)</client>";
+
+        private List<User> userList = new List<User>();
+        private User selectedUser = null;
 
         HellmanKeyExchange Exchanger;
         Encryption encrypt = new Encryption();
@@ -32,26 +43,198 @@ namespace Simple_Chat
 
             Exchanger = new HellmanKeyExchange();
             PrivateKey = Exchanger.initPrivateKey(encrypt.AlphabetNew());
-            PublicKey = Exchanger.generatePublicKey(PrivateKey, encrypt.AlphabetNew());
-            bool not_connected = true;
-            while (not_connected)
-            {
-                try
-                {
-                    /* to test the software just use the chat server from my Github repostiory 
-                     * https://github.com/IRayofficial/Chat_Server.git
-                     * and run it localy or run it on a Server but if you do change the adress to the internal or external
-                     * server adress
-                     */
-                    TcpClient client = new TcpClient("localhost", 8888); 
-                    stream = client.GetStream();
+            PublicKey = Exchanger.generatePublicKey(PrivateKey, encrypt.AlphabetNew());            
+        }
 
-                    Task.Run(() => ReceiveMessages(stream));
-                    not_connected = false;
-                }
-                catch (Exception ex) { }
+        //Connections Click
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(UserName.Text)) {
+                MyUserName = UserName.Text;
+                Task.Run(() => connectToServer());
             }
+            else
+            {
+                ConnectError.Text = "Username cannot be empty";
+            }
+        }
+
+        private async Task connectToServer()
+        {
+            try
+            {
+                /* to test the software just use the chat server from my Github repostiory 
+                 * https://github.com/IRayofficial/Chat_Server.git
+                 * and run it localy or run it on a Server but if you do change the adress to the internal or external
+                 * server adress
+                 */
+                TcpClient client = new TcpClient("localhost", 8888);
+                stream = client.GetStream();
+
+                string connectionMessage = "<username>" + MyUserName + "</username><public-key>" + PublicKey + "</public-key>";
+                byte[] connectionMessageBytes = Encoding.UTF8.GetBytes(connectionMessage);
+                stream.Write(connectionMessageBytes, 0, connectionMessageBytes.Length);
+
+                connected = true;
+                
+                Dispatcher.Invoke(() =>
+                {
+                    LoginView.Visibility = Visibility.Hidden;
+                    ChatView.Visibility = Visibility.Visible;
+                });
+                Task.Run(() => ReceiveMessages(stream));
+            }
+            catch (Exception ex)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    ConnectError.Text = "Could not connect to Server";
+                });
+            }
+        }
+
+        //Event Listener for Clients
+        private async Task ReceiveMessages(NetworkStream stream)
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            try
+            {
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    string receivedMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    var matches = Regex.Matches(receivedMessage, RecievedContentPattern);
+                    string type = "";
+                    string content = "";
+                    foreach (Match match in matches)
+                    {
+                        if (match.Groups[1].Success) { type = match.Groups[1].Value; }
+                        if (match.Groups[2].Success) { content = match.Groups[2].Value; }
+                    }
+
+                    if (type == "USERLIST")
+                    {
+                        UpdateUserList(content);
+                    }
+                    else if (type == "MESSAGE")
+                    {
+                        string message = "";
+                        string user = "";
+                        var getMessage = Regex.Matches(content, GetMessage);
+                        foreach (Match match in getMessage)
+                        {
+                            if (match.Groups[1].Success) { user = match.Groups[1].Value; }
+                            if (match.Groups[2].Success) { message = match.Groups[2].Value; }
+
+                        }
+
+                        message = decryptMessage(message);
+                        Dispatcher.Invoke(() =>
+                        {
+                            AddMessageToStackPanel(user, message, Brushes.LightGreen, HorizontalAlignment.Left);
+                        });
+                    }
+                }
+            }
+            catch (Exception ex) { connected = false; }
+        }
+
+        //Updates the User List
+        private void UpdateUserList(string userListMessage)
+        {
+            List<string> users = new List<string>();
+            var matches = Regex.Matches(userListMessage, GetClient);
+
+            foreach (Match match in matches)
+            {
+                if (match.Groups[1].Success)
+                {
+                    users.Add(match.Groups[1].Value);
+                }
+            }
+
+            userList.Clear();
+
+            foreach (var user in users)
+            {
+                if (!string.IsNullOrEmpty(user))
+                {
+                    var userInfo = Regex.Matches(user, GetUserInfo);
+                    string userName = "";
+                    string publicKey = "";
+                    foreach (Match match in userInfo)
+                    {
+                        if (match.Groups[1].Success)
+                        {
+                            userName = match.Groups[1].Value;
+                        }
+                        if (match.Groups[2].Success)
+                        {
+                            publicKey = match.Groups[2].Value;
+                        }
+                    }
+                    User newUser = new User(userName, publicKey);
+                    userList.Add(newUser);
+                }
+            }
+
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                UserListPanel.Children.Clear();
+
+                foreach (var user in userList)
+                {
+                    if (user.PublicKey != PublicKey)
+                    {
+                        Border userBox = new Border
+                        {
+                            Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#CCCCCC")),
+                            CornerRadius = new CornerRadius(10),
+                            Padding = new Thickness(10),
+                            Margin = new Thickness(5),
+                            BorderBrush = Brushes.Black,
+                            BorderThickness = new Thickness(1),
+                        };
+
+                        StackPanel infoPanel = new StackPanel();
+
+                        TextBlock usernameBlock = new TextBlock
+                        {
+                            Text = user.UserName,
+                            FontWeight = FontWeights.Bold,
+                            Foreground = Brushes.DarkBlue,
+                            Margin = new Thickness(0, 0, 0, 5)
+                        };
+
+                        TextBlock publicKeyBlock = new TextBlock
+                        {
+                            Text = user.PublicKey,
+                        };
+
+                        infoPanel.Children.Add(usernameBlock);
+                        infoPanel.Children.Add(publicKeyBlock);
+
+                        // Add an event to select this user for private chat
+                        userBox.MouseDown += (s, e) => SelectUserForPrivateChat(user);
+
+                        userBox.Child = infoPanel;
+
+                        UserListPanel.Children.Add(userBox); // Add user to the list in the UI
+                    }
+                }
+
+            }));
+        }
+
+        //Select User click even
+        private void SelectUserForPrivateChat(User user)
+        {
+            selectedUser = user; // Store the selected user
+                                 // Optionally update UI to show the selected user
             
+            calculateSharedKey(user.PublicKey);
+
+            SelectedUserLabel.Text = "Chatting with: " + user.UserName;
         }
 
         //Ecryption methods
@@ -62,12 +245,19 @@ namespace Simple_Chat
 
         private string encryptMessage(string message)
         {
-            return new string(encrypt.encodeKey(message, SharedKey));
+            if (!string.IsNullOrEmpty(SharedKey))
+            {
+                return new string(encrypt.encodeKey(message, SharedKey));
+            }
+            return message;
         }
 
         private string decryptMessage(string message)
         {
-            return new string(encrypt.decodeKey(message, SharedKey));
+            if (!string.IsNullOrEmpty(SharedKey)) { 
+                return new string(encrypt.decodeKey(message, SharedKey));
+            }
+            return message;
         }
 
         //Send A Message
@@ -75,32 +265,23 @@ namespace Simple_Chat
         {
             string messageText = MessageInput.Text;
 
-            if (!string.IsNullOrEmpty(messageText))
+            if (!string.IsNullOrEmpty(messageText) && selectedUser != null)
             {
-                byte[] buffer = Encoding.ASCII.GetBytes(messageText);
-                stream.Write(buffer, 0, buffer.Length);
+                int userId = userList.IndexOf(selectedUser);
+                messageText = encryptMessage(messageText);
+                string sendString = "<from>"+MyUserName+"</from><content>" + messageText + "</content><send-to>" +userId+ "</send-to>"; // TODO MAKE ADDRESSATION TO CORRECT USER
 
-                //This Client
-                AddMessageToStackPanel("User1", messageText, Brushes.LightBlue, HorizontalAlignment.Right);
-
-                MessageInput.Text = string.Empty;
-                ScrollToBottom();
-            }
-        }
-
-        //Event Listener for Clients
-        private void ReceiveMessages(NetworkStream stream)
-        {
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-
-            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
-            {
-                string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-                Dispatcher.BeginInvoke(new Action(() =>
+                byte[] buffer = Encoding.UTF8.GetBytes(sendString);
+                try
                 {
-                    AddMessageToStackPanel("User2", message, Brushes.LightGreen, HorizontalAlignment.Left);
-                }));
+                    stream.Write(buffer, 0, buffer.Length);
+                    messageText = decryptMessage(messageText);
+                    AddMessageToStackPanel(MyUserName, messageText, Brushes.LightBlue, HorizontalAlignment.Right);
+
+                    MessageInput.Text = string.Empty;
+                    ScrollToBottom();
+                }
+                catch (Exception ex) { connected = false; }   
             }
         }
 
@@ -146,5 +327,7 @@ namespace Simple_Chat
         {
             ChatScrollViewer.ScrollToEnd();
         }
+
+
     }
 }
---
+++ Simple_Chat/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Chat
+{
+    internal class User
+    {
+        public string UserName { get; set; }
+        public string PublicKey { get; set; }
+
+        public User(string userName, string publicKey)
+        {
+            UserName = userName;
+            PublicKey = publicKey;
+        }
+    }
+}
